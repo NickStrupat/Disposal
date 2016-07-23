3b01884a7dae3edf45c84babfc6421c47461c2b8
Nick Strupat <nick.strupat@gmail.com>
2016-07-22 20:56:03 -0400

    comments for fogettable logic

--- src/Disposal/Helpers.cs
+++ src/Disposal/Helpers.cs
@@ -18,9 +18,11 @@ namespace Disposal {
 		public static Boolean MarkDisposed(ref Int32 useCount) {
 			var spinWait = new SpinWait();
 			for (;;) {
-				var original = Interlocked.CompareExchange(ref useCount, Disposed, Disposable);
+				var original = Interlocked.CompareExchange(ref useCount, Disposed, Disposable); // set useCount to Disposed if is currently Disposable
 				if (original == Disposable)
 					return true;
+				// Check if less-than 0 because a user could mess up the calls to EnterGuard and ExitGuard by not correctly putting them in
+				// a try-finally block (see DisposalGuard methods). This gives us room for (Int.MaxValue - 1) bad calls to Enter/ExitGuard
 				if (original < Disposable)
 					return false;
 				spinWait.SpinOnce();
